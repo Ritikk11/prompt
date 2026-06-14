92fb25233180422348ce3130eda4c61ea5622e8d
Ritik <ritik@example.com>
2026-06-14 22:49:03 +0530

    Remove guides blog homepage block

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { fetchSections, fetchSettings, fetchPostSummaries, getPostsForSection } 
 import FeaturedSlider from '@/components/FeaturedSlider';
 import HomeSection from '@/components/HomeSection';
 import HomeLinkBlocks from '@/components/HomeLinkBlocks';
-import HomeEditorialSections from '@/components/HomeEditorialSections';
 
 
 export default async function Home() {
@@ -29,7 +28,6 @@ export default async function Home() {
       </section>
 
       <HomeLinkBlocks blocks={settings.homeLinkBlocks} />
-      <HomeEditorialSections posts={allPosts} />
 
       {/* Main Content */}
       {homepageSections.map((section, idx) => (
